bdb2bcc081ef7ea4411ccf2f75aba0b9dab94e0f
Meison Lee <meisonlee2001@gmail.com>
2025-06-19 17:31:44 +0800

    Add RoundRobin test

--- HttpProxy/internal/handlers/proxy_test.go
+++ HttpProxy/internal/handlers/proxy_test.go
@@ -5,6 +5,7 @@ import (
 	"Go_Playground/HttpProxy/internal/loadbalancer"
 	"Go_Playground/HttpProxy/internal/model"
 	"Go_Playground/HttpProxy/internal/state"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -144,5 +145,71 @@ func TestProxyHandler_BackendTimeout(t *testing.T) {
 
 	if res.StatusCode != http.StatusGatewayTimeout {
 		t.Errorf("expected status 504 for backend timeout, got %d", res.StatusCode)
+	}
+}
+
+func TestProxyHandler_RoundRobin(t *testing.T) {
+
+	backend1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("backend1"))
+	}))
+	defer backend1.Close()
+
+	backend2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("backend2"))
+	}))
+	defer backend2.Close()
+
+	state.Initialize()
+
+	backends := []model.Backend{
+		{
+			URL:    backend1.URL,
+			Weight: 1,
+			Health: true,
+		},
+		{
+			URL:    backend2.URL,
+			Weight: 1,
+			Health: true,
+		},
+	}
+
+	lb := loadbalancer.NewRoundRobinLoadBalancer(backends)
+	routePool := &items.RoutePool{
+		Backends:     backends,
+		LoadBalancer: lb,
+	}
+
+	state.Routes["/test"] = routePool
+
+	attempts := 10
+	for i := range attempts {
+		req := httptest.NewRequest(http.MethodGet, "/test/abc", nil)
+		w := httptest.NewRecorder()
+
+		ProxyHandler(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if res.StatusCode != http.StatusOK {
+			t.Errorf("expected status 200, got %d", res.StatusCode)
+		}
+
+		if i%2 == 0 && string(body) != "backend1" {
+			t.Errorf("expected backend1, got %s", string(body))
+		}
+
+		if i%2 == 1 && string(body) != "backend2" {
+			t.Errorf("expected backend2, got %s", string(body))
+		}
 	}
 }
